c27ebf3c1b622302cdb745a7df03fd635ed79383
Yangkai.Shen <237497819@qq.com>
2019-03-05 14:59:41 +0800

    :tada: 初始化 hello-spring-boot-starter

---
+++ src/main/java/com/xkcoding/hello/HelloSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.xkcoding.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBootStarterApplication.class, args);
+    }
+
+}
